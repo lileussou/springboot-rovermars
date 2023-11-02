094acb2110cdf43c74756ee1768050479c8e6947
lileussou <cameron.cisse@live.fr>
2023-11-02 18:00:58 +0100

    Initial commit

---
+++ src/test/java/com/rovermars/springbootrovermars/SpringbootRovermarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rovermars.springbootrovermars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRovermarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
